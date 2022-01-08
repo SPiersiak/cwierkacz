34df0f73128ce6785813a7038df20f25ac606a32
Szymon <piersiak.szymon@gmail.com>
2022-01-08 22:33:49 +0100

    added the ability to add and update a profile picture

--- cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/ProfileActivity.kt
+++ cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/ProfileActivity.kt
@@ -1,25 +1,26 @@
 package com.example.cwierkaczapp
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import com.example.cwierkaczapp.util.DATA_USERS
-import com.example.cwierkaczapp.util.DATA_USER_EMAIL
-import com.example.cwierkaczapp.util.DATA_USER_USERNAME
-import com.example.cwierkaczapp.util.User
+import com.example.cwierkaczapp.util.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
-
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val firebaseDB = FirebaseFirestore.getInstance()
+    private val firebaseStorage = FirebaseStorage.getInstance().reference
     private val userId = FirebaseAuth.getInstance().currentUser?.uid
+    private var imageUrl: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +32,12 @@ class ProfileActivity : AppCompatActivity() {
 
         profileProgressLayout.setOnTouchListener { v, event -> true }
 
+        photoIV.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_PHOTO)
+        }
+
         populateInfo()
     }
 
@@ -41,7 +48,10 @@ class ProfileActivity : AppCompatActivity() {
                 val user = documentSnapshot.toObject(User::class.java)
                 usernameET.setText(user?.username, TextView.BufferType.EDITABLE)
                 emailET.setText(user?.email, TextView.BufferType.EDITABLE)
-
+                imageUrl = user?.imageUrl
+                imageUrl?.let {
+                    photoIV.loadUrl(user?.imageUrl, R.drawable.logo)
+                }
                 profileProgressLayout.visibility = View.GONE
             }
             .addOnFailureListener { e ->
@@ -50,7 +60,7 @@ class ProfileActivity : AppCompatActivity() {
             }
     }
 
-    fun onApply(v:View){
+    fun onApply(v: View) {
         profileProgressLayout.visibility = View.VISIBLE
         val username = usernameET.text.toString()
         val email = emailET.text.toString()
@@ -69,10 +79,52 @@ class ProfileActivity : AppCompatActivity() {
                 profileProgressLayout.visibility = View.GONE
             }
     }
-    fun onSignout(v:View){
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PHOTO) {
+            storeImage(data?.data)
+        }
+    }
+
+    fun storeImage(imageUri: Uri?) {
+        imageUri?.let {
+            Toast.makeText(this, "Uploading...", Toast.LENGTH_SHORT).show()
+            profileProgressLayout.visibility = View.VISIBLE
+
+            val filePath = firebaseStorage.child(DATA_IMAGES).child(userId!!)
+            filePath.putFile(imageUri)
+                .addOnSuccessListener {
+                    filePath.downloadUrl
+                        .addOnSuccessListener {uri ->
+                            val url = uri.toString()
+                            firebaseDB.collection(DATA_USERS).document(userId!!).update(DATA_USER_IMAGE_URL, url)
+                                .addOnSuccessListener {
+                                    imageUrl = url
+                                    photoIV.loadUrl(imageUrl, R.drawable.logo)
+                                }
+                            profileProgressLayout.visibility = View.GONE
+                        }
+                        .addOnFailureListener {
+                            onUploadFailure()
+                        }
+                }
+                .addOnFailureListener {
+                    onUploadFailure()
+                }
+        }
+    }
+
+    fun onUploadFailure() {
+        Toast.makeText(this, "Image upload failed. Please try agail later.", Toast.LENGTH_SHORT).show()
+        profileProgressLayout.visibility = View.GONE
+    }
+
+    fun onSignout(v: View) {
         firebaseAuth.signOut()
         finish()
     }
+
     companion object {
         fun newIntent(context: Context) = Intent(context, ProfileActivity::class.java)
     }
