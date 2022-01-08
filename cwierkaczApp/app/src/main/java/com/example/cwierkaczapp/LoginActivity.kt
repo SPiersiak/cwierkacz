a868f94d3f9096c80244cadb88b857c12d1434ae
Szymon <piersiak.szymon@gmail.com>
2022-01-08 15:47:55 +0100

    added logout button

--- cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/LoginActivity.kt
+++ cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cwierkaczapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -87,5 +89,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         firebaseAuth.removeAuthStateListener(firebaseAuthListener)
+    }
+
+    companion object {
+        fun newIntent(context: Context) = Intent(context, LoginActivity::class.java)
     }
 }
