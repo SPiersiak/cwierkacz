438f6f7a038e03b1273fdebe8852ab470e746066
Sebastian Poweska <spoweska98@gmail.com>
2022-01-10 16:09:02 +0100

    my activity screen structure and functionality

--- cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/fragments/MyActivityFragment.kt
+++ cwierkaczApp/app/src/main/java/com/example/cwierkaczapp/fragments/MyActivityFragment.kt
@@ -5,7 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cwierkaczapp.R
+import com.example.cwierkaczapp.adapters.TweetListAdapter
+import com.example.cwierkaczapp.listeners.TwitterListenerImpl
+import com.example.cwierkaczapp.util.DATA_TWEETS
+import com.example.cwierkaczapp.util.DATA_TWEET_USER_IDS
+import com.example.cwierkaczapp.util.Tweet
+import kotlinx.android.synthetic.main.fragment_my_activity.*
 
 
 class MyActivityFragment : TwitterFragment() {
@@ -19,7 +27,47 @@ class MyActivityFragment : TwitterFragment() {
         return inflater.inflate(R.layout.fragment_my_activity, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        listener = TwitterListenerImpl(tweetList, currentUser, callback)
+
+        tweetsAdapter = TweetListAdapter(userId!!, arrayListOf())
+        tweetsAdapter?.setListener(listener)
+        tweetList?.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = tweetsAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        }
+
+        swipeRefresh.setOnRefreshListener {
+            swipeRefresh.isRefreshing = false
+            updateList()
+        }
+    }
+
+
     override fun updateList() {
+        tweetList?.visibility = View.GONE
+        val tweets = arrayListOf<Tweet>()
+
+        firebaseDB.collection(DATA_TWEETS).whereArrayContains(DATA_TWEET_USER_IDS, userId!!).get()
+            .addOnSuccessListener { list ->
+                for (document in list.documents) {
+                    val tweet = document.toObject(Tweet::class.java)
+                    tweet?.let {
+                        tweets.add(tweet)
+                    }
+                }
+
+                val sortedList = tweets.sortedWith(compareByDescending { it.timestamp })
+                tweetsAdapter?.updateTweets(sortedList)
+                tweetList?.visibility = View.VISIBLE
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+                tweetList?.visibility = View.VISIBLE
+            }
 
     }
 
